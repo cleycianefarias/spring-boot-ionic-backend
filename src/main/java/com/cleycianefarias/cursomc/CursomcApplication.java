4811d7d5f0a147b8b513cbf90224fa1d25ebed7c
Cleyciane Farias <fariascleycianedelima@gmail.com>
2020-07-17 14:23:07 -0500

    Estado e cidade

--- src/main/java/com/cleycianefarias/cursomc/CursomcApplication.java
+++ src/main/java/com/cleycianefarias/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cleycianefarias.cursomc.domain.Categoria;
+import com.cleycianefarias.cursomc.domain.Cidade;
+import com.cleycianefarias.cursomc.domain.Estado;
 import com.cleycianefarias.cursomc.domain.Produto;
 import com.cleycianefarias.cursomc.repositories.CategoriaRepository;
+import com.cleycianefarias.cursomc.repositories.CidadeRepository;
+import com.cleycianefarias.cursomc.repositories.EstadoRepository;
 import com.cleycianefarias.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRespository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	
 	public static void main(String[] args) {
@@ -42,9 +50,22 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+
 		categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 		
 		
