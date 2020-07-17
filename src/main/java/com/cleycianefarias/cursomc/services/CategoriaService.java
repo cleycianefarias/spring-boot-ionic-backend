d2cbbe0caeb3cb5d11c48f43faccd1941ceb1d25
Cleyciane Farias <fariascleycianedelima@gmail.com>
2020-07-16 22:13:13 -0500

    Criando repository e service para categoria

---
+++ src/main/java/com/cleycianefarias/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.cleycianefarias.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cleycianefarias.cursomc.domain.Categoria;
+import com.cleycianefarias.cursomc.repositories.CategoriaRepository;
+import java.util.Optional;
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+}
