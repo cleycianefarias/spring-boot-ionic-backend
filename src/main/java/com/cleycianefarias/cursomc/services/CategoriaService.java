40b338b95cc44e572004c5bda805c954fe5b742b
Cleyciane Farias <fariascleycianedelima@gmail.com>
2020-07-29 13:06:37 -0500

    Validacao sintatica com o Bean Validation

--- src/main/java/com/cleycianefarias/cursomc/services/CategoriaService.java
+++ src/main/java/com/cleycianefarias/cursomc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cleycianefarias.cursomc.domain.Categoria;
+import com.cleycianefarias.cursomc.dto.CategoriaDTO;
 import com.cleycianefarias.cursomc.repositories.CategoriaRepository;
 import com.cleycianefarias.cursomc.services.exceptions.DataIntegrityException;
 import com.cleycianefarias.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
