948581396971f3b8555740a8cfeb777f9db2c67f
Cleyciane Farias <fariascleycianedelima@gmail.com>
2020-07-31 09:21:24 -0500

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/cleycianefarias/cursomc/services/CategoriaService.java
+++ src/main/java/com/cleycianefarias/cursomc/services/CategoriaService.java
@@ -32,10 +32,12 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -59,5 +61,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
